a3f93552ee54730bedaddb79b1264af48744cffc
Alwin <alwinjohn231@gmail.com>
2022-10-26 13:54:40 +0530

    update: lexical analayzer added

---
+++ Lexical-Analayzer/lexical.c
@@ -0,0 +1,101 @@
+/*
+Create an input.txt with your implementation of any c-program
+Then run the lexical.c using gcc
+*/
+
+
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+
+int main()
+{
+
+	FILE *input, *output;
+	int l=1;
+	int t=0;
+	int j=0;
+	int i,flag;
+	char ch,str[20];
+	input = fopen("input.txt","r");
+	output = fopen("output.txt","w");
+	char keyword[30][30] = {"int","main","if","else","do","while"};
+	fprintf(output,"Line no. \t Token no. \t\t Token \t\t Lexeme\n\n");
+
+	while(!feof(input))
+	{
+		i=0;
+		flag=0;
+		ch=fgetc(input);
+
+		if( ch=='+' || ch== '-' || ch=='*' || ch=='/' )
+		{
+			fprintf(output,"%7d\t\t %7d\t\t Operator\t %7c\n",l,t,ch);
+			t++;
+		}
+
+		else if( ch==';' || ch=='{' || ch=='}' || ch=='(' || ch==')' || ch=='?' || ch=='@' ||ch=='!' || ch=='%')
+
+		{
+
+			fprintf(output,"%7d\t\t %7d\t\t Special symbol\t %7c\n",l,t,ch);
+			t++;
+		}
+
+		else if(isdigit(ch))
+		{
+			fprintf(output,"%7d\t\t %7d\t\t Digit\t\t %7c\n",l,t,ch);
+			t++;
+		}
+
+		else if(isalpha(ch))
+		{
+			str[i]=ch;
+			i++;
+			ch=fgetc(input);
+
+			while(isalnum(ch) && ch!=' ')
+			{
+				str[i]=ch;
+				i++;
+				ch=fgetc(input);
+			}
+
+			str[i]='\0';
+
+			for(j=0;j<=30;j++)
+
+			{
+				if(strcmp(str,keyword[j])==0)
+				{
+					flag=1;
+					break;
+				}
+			}
+
+			if(flag==1)
+			{
+				fprintf(output,"%7d\t\t %7d\t\t Keyword\t %7s\n",l,t,str);
+				t++;
+			}
+
+			else
+			{
+				fprintf(output,"%7d\t\t %7d\t\t Identifier\t %7s\n",l,t,str);
+				t++;
+			}
+
+		}
+
+		else if(ch=='\n')
+
+		{
+			l++;
+		}
+	}
+
+	fclose(input);
+	fclose(output);
+	return 0;
+
+	}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Compiler-Design-Lab-S7
+<h1 align="center">
+    CSL411 Compiler Lab
+</h1>
+
+
+<div align="center">
+    This repository contains a collection of programs written as part of KTU CSL332 Networking Lab 
+</div>
+
+
+### Compiler Design Programs
+
+[Lexical Analayzer](/Lexical-Analayzer)
